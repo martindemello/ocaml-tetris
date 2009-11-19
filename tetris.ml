5c6803477c5abe77208e596539dc96aa54d29884
Martin DeMello <martindemello@gmail.com>
2009-11-19 20:04:52 +0530

    pure update_board function that doesn't draw to screen

--- tetris.ml
+++ tetris.ml
@@ -37,7 +37,7 @@ let default_configuration = {
   fall_delay = 0.200 (* s *)
 }
 
-let pixel_dimensions c = (c.m * c.p + 100, c.n * c.q + 200)
+let pixel_dimensions c = (2 * c.m * c.p + 200, c.n * c.q + 200)
 
 (* let fall_delay = 0.1250000 (* s *) *)
 let compact_delay = 0.250 (* s *)
@@ -143,6 +143,174 @@ let initial_state c =
 
 let sf = Printf.sprintf
 let debug msg = Printf.eprintf "debug: %s\n" msg; flush stderr
+
+let update_board c q t k =
+  let k = match k with
+  | Some(Keypad.Pressed Keypad.Left) -> Some(Left)
+  | Some(Keypad.Pressed Keypad.Right) -> Some(Right)
+  | Some(Keypad.Pressed Keypad.Down) -> Some(Down)
+  | Some(Keypad.Pressed Keypad.Up) -> Some(Rotate)
+  | Some(Keypad.Pressed Keypad.Start) -> Some(Pause)
+  | Some(Keypad.Pressed Keypad.L) -> Some(Down)
+  | Some(Keypad.Pressed Keypad.R) -> Some(Down)
+  | Some(Keypad.Pressed Keypad.Select) -> Some(Drop)
+  | _ -> None
+  in
+  let reset_game () =
+	q.what <- Falling c.fall_delay;
+	for i = 0 to c.m - 1 do
+	  for j = 0 to c.n - 1 do
+		q.board.(i).(j) <- None
+	  done
+	done;
+    q.score <- 0;
+    q.lines <- 0;
+	q.future <- random_tile ();
+	q.present <- random_tile ();
+	q.position <- (1,c.n/2)
+  in
+  let occupied i j =
+	(i < 0 or j < 0 or i >= c.m or j >= c.n or
+	 q.board.(i).(j) <> None)
+  in
+  let might_occupy t i j = List.for_all (fun (di,dj) -> not (occupied (i + di) (j + dj))) (stones_of_tile t) in
+  let carve_stone t i j =
+	let c = Color.Shade((Random.float (2.0 *. shade_variation)) -. shade_variation, Color.Dark(color_of_tile t)) in
+	List.iter (fun (di,dj) -> q.board.(i + di).(j + dj) <- Some c) (stones_of_tile t)
+  in
+  let next_tile () =
+	q.present <- q.future;
+	q.future <- random_tile ();
+	let (i,j) = (1,c.n/2) in
+	q.position <- (i,j);
+	if might_occupy q.present i j then
+	  begin
+		q.what <- Falling(q.delay);
+	  end
+	else
+	  begin
+		q.what <- Dead;
+	  end
+  in
+  (* move the piece one position down *)
+  (* returns true iff no restriction on horizontal motion could give a valid position, i.e. bottom was reached *)
+  let move_piece di k =
+	(* piece falls *)
+	(* decrease by one *)
+	let (i,j) = q.position in
+	let i = i + di in
+	let (i,j) =
+	  match k with
+		Some Left when might_occupy q.present i (j - 1) -> (i,j - 1)
+	  | Some Right when might_occupy q.present i (j + 1) -> (i,j + 1)
+	  | _ -> (i,j)
+	in
+	if might_occupy q.present i j then
+	  begin
+		q.position <- (i,j);
+		false
+	  end
+	else
+	  true
+  in
+  let rec check i j = j = c.n or (q.board.(i).(j) <> None && check i (j + 1)) in
+  let do_compaction () =
+	let rec loop r i =
+	  if i = c.m then
+		r
+	  else
+		if check i 0 then
+		  loop r (i + 1)
+		else
+		  loop (i::r) (i + 1)
+	in
+	let rec loop' i = function
+		i'::r ->
+		  Array.blit q.board.(i') 0 q.board.(i - 1) 0 c.n;
+		  loop' (i - 1) r
+	  | [] -> loop'' i
+	and loop'' i =
+	  if i = 0 then
+		()
+	  else
+		begin
+		  Array.fill q.board.(i - 1) 0 c.n None;
+		  loop'' (i - 1)
+		end
+	in
+	loop' c.m (loop [] 0);
+	next_tile ()
+  in
+  let reached_bottom () =
+    (* piece reached bottom *)
+	begin
+	  let (i,j) = q.position in
+	  carve_stone q.present i j;
+	  (* check if any lines are completed *)
+	  let rec loop r i =
+		if i = c.m then
+		  r
+		else
+		  if check i 0 then
+			loop (i::r) (i + 1)
+		  else
+			loop r (i + 1)
+	  in
+	  match loop [] 0 with
+	  | [] -> next_tile ()
+	  | l ->
+          let r = List.length l in
+          q.lines <- r + q.lines;
+          q.score <- r * r + q.score;
+		  q.what <- Compacting(compact_delay,l);
+	end
+  in
+  let as_usual t =
+	q.what <- Falling(t);
+  in
+  let attempt_to_rotate_piece ccw =
+	let (r,t) = q.present in
+	let r' = if ccw then counter_clockwise r else clockwise r in
+	let (i,j) = q.position in
+	if might_occupy (r',t) i j then
+	  q.present <- (r',t)
+	else
+	  ()
+  in
+  match (q.what,k) with
+  | (Paused c,Some Pause) ->
+	  q.what <- c;
+  | (Paused _,_) -> ()
+  | (_,Some Pause) ->
+	  q.what <- Paused q.what;
+  | (Falling x,Some Drop) ->
+      while not (move_piece 1 k) do
+        ()
+      done;
+      reached_bottom ()
+  | (Falling x,_) ->
+	  let di = match k with Some Down -> 1 | _ -> 0 in
+	  begin
+		match k with
+		  Some Rotate -> attempt_to_rotate_piece true
+		| Some Rotate' -> attempt_to_rotate_piece false
+		| _ -> ()
+	  end;
+	  if t < x then
+		if move_piece (max 0 di) k then reached_bottom () else as_usual (x -. t)
+	  else
+		if move_piece 1 k then reached_bottom () else as_usual q.delay
+  | (Compacting(x,l),None) ->
+	  if t < x then
+		begin
+		  q.what <- Compacting(x -. t,l);
+		end
+	  else
+		do_compaction ()
+  | (Compacting(_,_),_) -> do_compaction ()
+  | (Dead,Some Drop) ->
+	  reset_game ();
+  | (Dead,_) -> ()
 
 let transition c q t k =
   let k = match k with
@@ -373,14 +541,14 @@ let transition c q t k =
 	  (draw_board (), Some q.delay)
   | (Dead,_) -> (draw_board (), None)
 
-(* screen dimensions *)
-let width = 1000
-let height = 400
 
-let () =
-  allegro_init();
-  install_keyboard();
+let corner x y = 10 + 20 * x, 10 + 20 * y
 
+let coords x y =
+  let x1, y1 = corner x y in
+  x1+1, y1+1, x1+19, y1+19
+
+let init_screen width height =
   begin
     try set_gfx_mode GFX_AUTODETECT_WINDOWED width height 0 0;
     with _ ->
@@ -389,20 +557,33 @@ let () =
         set_gfx_mode GFX_TEXT 0 0 0 0;
         allegro_message("Unable to set any graphic mode\n"^ (get_allegro_error()) ^"\n");
         exit 1;
-  end;
+  end
+
+let () =
+  let cfg = default_configuration in
+  let (width, height) = pixel_dimensions cfg in
+  let q = initial_state cfg in
+  allegro_init();
+  install_keyboard();
+  install_timer();
+  init_screen width height;
 
   set_palette(get_desktop_palette());
 
   let screen = get_screen() in
   clear_to_color screen (makecol 0 0 0);
 
-  (* you don't need to do this, but on some platforms (eg. Windows) things
-   * will be drawn more quickly if you always acquire the screen before
-   * trying to draw onto it.
-   *)
   acquire_screen();
 
   let (screen_w, screen_h) = get_screen_width(), get_screen_height() in
+
+  for i = 0 to cfg.m do
+    for j = 0 to cfg.n do
+      let col = (makecol 100 100 100) in
+      let x1, y1, x2, y2 = coords i j in
+      rectfill screen x1 y1 x2 y2 col
+    done
+  done;
 
   (* hello world *)
   textout_centre_ex screen (get_font()) "Hello, world!" (screen_w/2) (screen_h/2) (makecol 100 100 100) (color_index(-1));
